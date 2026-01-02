adf65c8b80d84a2a8ac806f73fe0a69c6aa53a1c
haiderarshad021 <haiderarshad021@gmail.com>
2026-01-02 09:54:33 +0500

    updated session timeout

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,7 +14,7 @@ const login = async (req, res, next) => {
     const token = jwt.sign(
       { userId: user.id, role: user.role },
       process.env.JWT_SECRET,
-      { expiresIn: "1h" }
+      { expiresIn: "30m" }
     );
 
     res.json({ token, user: { id: user.id, username, role: user.role } });
